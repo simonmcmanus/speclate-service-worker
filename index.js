4d468c8c4623cdd9fb297cfb070f57f7f773660f
Simon McManus <mcmanus.simon@gmail.com>
2016-09-08 19:15:34 +0100

    tidy up

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var swSortFiles = require('./sort-files');
+var url = require('url');
 
-module.exports = function(spec, version, extraFiles) {
+module.exports = function(spec, version) {
 
     var out = swSortFiles(spec);
 
+
+    console.log('sorted files', out);
     var cacheName = 'v' + version + '::';
     var self = this;
     self.addEventListener('activate', event => {
-         //console.log('activated');
+         console.log('activated');
     });
 
     self.addEventListener('install', e => {
@@ -16,19 +19,25 @@ module.exports = function(spec, version, extraFiles) {
         e.waitUntil(
             [
                 caches.open(cacheName + 'layout').then(cache => {
+                    // hash
                     return cache.addAll(out.layout);
                 }),
                 caches.open(cacheName + 'components').then(cache => {
+                    // hash
                     return cache.addAll(out.components);
                 }),
                 caches.open(cacheName + 'pages').then(cache => {
+                    // hash
                     return cache.addAll(out.pages);
                 }),
                 caches.open(cacheName + 'specs').then(cache => {
+                    // can these be version?
                     return cache.addAll(out.specs);
                 }),
                 caches.open(cacheName + 'extras').then(cache => {
-                    return cache.addAll(extraFiles);
+
+                    // these files should be hashed.
+                    return cache.addAll(out.extras);
                 }),
                 caches.open(cacheName + 'routes').then(cache => {
 
@@ -38,23 +47,23 @@ module.exports = function(spec, version, extraFiles) {
 
                             cache.put(route, layout.clone());
                         })
-
                     });
-
                 })
             ]
         )
-
     })
 
-
-        // when the browser fetches a url, either response with
+    // when the browser fetches a url, either response with
     // the cached object or go ahead and fetch the actual url
     self.addEventListener('fetch', event => {
         var request = event.request;
-                //return  event.respondWith(caches.match('/pages/layout.html'));
+        var urlObj = url.parse(request.url);
 
-        if (request.url.indexOf('/api/speclate') > 0) {
+
+        var key = urlObj.pathname.slice(1);
+
+
+       if (request.url.indexOf('/api/speclate') > 0) {
             return event.respondWith(
                 // try the network first
                 fetch(request)
@@ -65,13 +74,28 @@ module.exports = function(spec, version, extraFiles) {
                         return caches.match(request).then(response => response);
                     })
             );
-        //} else if (request.url === '/speak.html') {
-//            return  event.respondWith(caches.match('/pages/layout.html'));
-        }else {
+        } else {
+            // return  fetch(event.request).catch((e) => {
+            //     console.log('caught', e)
+            // })
             return event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
         }
     });
 
+
+}
+// remove old keys from the cache
+var cleanup = function(cacheKey) {
+
+    caches.keys().then(function(cacheNames) {
+    return Promise.all(
+        cacheNames.map(function(cacheName) {
+        if(cacheName.slice(0, cacheKey.length) != cacheKey) {
+            return caches.delete(cacheName);
+        }
+        })
+    );
+    });
 }
 
 
