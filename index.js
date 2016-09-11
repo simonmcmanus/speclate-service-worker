d0968ee36c34574bfcf60e4c6da0c0772b4bc200
Simon McManus <mcmanus.simon@gmail.com>
2016-09-11 10:07:53 +0100

    linting fixes

--- index.js
+++ index.js
@@ -1,118 +1,92 @@
-var swSortFiles = require('./sort-files');
-var url = require('url');
+var swSortFiles = require('./sort-files')
 
-module.exports = function(spec, version) {
-    spec = spec.options.scanSpecForFiles(spec);
-    var out = swSortFiles(spec);
-    var cacheName = 'v' + version + '::';
-    var self = this;
-    self.addEventListener('activate', event => {
-          event.waitUntil(
-            caches.keys()
-            .then(function (keys) {
-                return Promise.all(keys
-                .filter(function (key) {
-                    return key.indexOf(cacheName) !== 0;
-                })
-                .map(function (key) {
-                    return caches.delete(key);
-                })
-                );
-            })
-        );
-        console.log('activated and cache updated');
-    });
+module.exports = function (spec, version) {
+  spec = spec.options.scanSpecForFiles(spec, true)
+  var out = swSortFiles(spec)
+  var cacheName = 'v' + version + '::'
+  var self = this
 
-    self.addEventListener('install', e => {
+  self.addEventListener('install', e => {
         // once the SW is installed, go ahead and fetch the resources
         // to make this work offline
-        e.waitUntil(
-            [
-                caches.open(cacheName + 'layout').then(cache => {
-                    return cache.addAll(out.layout);
-                }),
-                caches.open(cacheName + 'components').then(cache => {
-                    return cache.addAll(out.components);
-                }),
-                caches.open(cacheName + 'pages').then(cache => {
-                    return cache.addAll(out.pages);
-                }),
-                caches.open(cacheName + 'specs').then(cache => {
+    e.waitUntil(
+      [
+        caches.open(cacheName + 'layout').then(cache => {
+          return cache.addAll(out.layout)
+        }),
+        caches.open(cacheName + 'components').then(cache => {
+          return cache.addAll(out.components)
+        }),
+        caches.open(cacheName + 'pages').then(cache => {
+          return cache.addAll(out.pages)
+        }),
+        caches.open(cacheName + 'specs').then(cache => {
                     // can these be version?
-                    return cache.addAll(out.specs);
-                }),
-                caches.open(cacheName + 'extras').then(cache => {
-
+          return cache.addAll(out.specs)
+        }),
+        caches.open(cacheName + 'extras').then(cache => {
                     // these files should be hashed.
-                    return cache.addAll(out.extras);
-                }),
-                caches.open(cacheName + 'routes').then(cache => {
-
-                    fetch('/pages/layout.html').then(function(layout) {
-
-                        out.routes.forEach(function(route) {
-
-                            cache.put(route, layout.clone());
-                        })
-                    });
-                })
-            ]
+          return cache.addAll(out.extras)
+        }),
+        caches.open(cacheName + 'routes').then(cache => {
+          fetch('/pages/layout.html').then(function (layout) {
+            out.routes.forEach(function (route) {
+              cache.put(route, layout.clone())
+            })
+          })
+        })
+      ]
         )
-    })
+  })
 
     // when the browser fetches a url, either response with
     // the cached object or go ahead and fetch the actual url
-    self.addEventListener('fetch', event => {
-        var request = event.request;
-        var urlObj = url.parse(request.url);
+  self.addEventListener('fetch', event => {
+    var request = event.request
 
-
-        var key = urlObj.pathname.slice(1);
-
-
-       if (request.url.indexOf('/api/speclate') > 0) {
-            return event.respondWith(
+    if (request.url.indexOf('/api/speclate') > 0) {
+      return event.respondWith(
                 fetch(request)
-                    .then( response => response)
-                    .then( response => addToCache(cacheName + 'specs', request, response))
-                    .catch( () => {
+                    .then(response => response)
+                    .then(response => addToCache(cacheName + 'specs', request, response))
+                    .catch(() => {
                         // fallback to the cache.
-                        return caches
+                      return caches
                                 .match(request)
                                 .then(response => response)
                     })
-            );
-        } else {
-            return event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
-        }
-    });
+            )
+    } else {
+      return event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)))
+    }
+  })
 
-
+  self.addEventListener('activate', event => {
+    event.waitUntil(
+            caches.keys()
+            .then(function (keys) {
+              return Promise.all(keys
+                .filter(function (key) {
+                  return key.indexOf(cacheName) !== 0
+                })
+                .map(function (key) {
+                  return caches.delete(key)
+                })
+                )
+            })
+        )
+    console.log('activated and cache updated')
+  })
 }
-// remove old keys from the cache
-var cleanup = function(cacheKey) {
 
-    caches.keys().then(function(cacheNames) {
-    return Promise.all(
-        cacheNames.map(function(cacheName) {
-        if(cacheName.slice(0, cacheKey.length) != cacheKey) {
-            return caches.delete(cacheName);
-        }
-        })
-    );
-    });
-}
-
-
-var addToCache = function(cacheKey, request, response) {
-
+var addToCache = function (cacheKey, request, response) {
   if (response.ok) {
-    var copy = response.clone();
-    caches.open(cacheKey).then( cache => {
-      cache.put(request, copy);
-    });
-    return response;
+    var copy = response.clone()
+    caches.open(cacheKey).then(cache => {
+      cache.put(request, copy)
+    })
+    return response
   }
-  return false;
+  return false
+}
 
-};
