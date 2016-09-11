9b9b7cd833b9d3f09d3e0731563aef8287e3989f
Simon McManus <mcmanus.simon@gmail.com>
2016-09-11 10:59:55 +0100

    standard --fix

--- sort-files.js
+++ sort-files.js
@@ -1,28 +1,27 @@
-module.exports = function(spec) {
-
-  var layout = [ '/pages/layout.html' ];
-  var components = [];
-  var pages = [];
-  var routes = ['/'];
-  var specs = [];
+module.exports = function (spec) {
+  var layout = [ '/pages/layout.html' ]
+  var components = []
+  var pages = []
+  var routes = ['/']
+  var specs = []
 
   Object.keys(spec).forEach(function (page) {
     // no de-duping going on - same page/component could be listed twice.
 
-    if(page === 'options') {
-        return;
+    if (page === 'options') {
+      return
     }
-    var pageName = spec[page].page;
-    var routeName = page.slice(0, -5);
-    routes.push(page);
-    pages.push('/pages/' + pageName + '/' + pageName + '.html' );
+    var pageName = spec[page].page
+    var routeName = page.slice(0, -5)
+    routes.push(page)
+    pages.push('/pages/' + pageName + '/' + pageName + '.html')
 
-    specs.push('/api/speclate' + routeName + '.json' );
+    specs.push('/api/speclate' + routeName + '.json')
     for (var selector in spec[page].spec) {
-      var component = spec[page].spec[selector].component;
-      components.push('/components/' + component + '/' + component + '.html');
+      var component = spec[page].spec[selector].component
+      components.push('/components/' + component + '/' + component + '.html')
     }
-  });
+  })
 
   return {
     components: components,
